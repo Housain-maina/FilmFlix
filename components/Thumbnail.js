639a2ca0dad0391e2ca6f7c204e97c25da707220
Hussaini Maina Usman <hussainmaina27@gmail.com>
2022-02-02 07:24:24 +0100

    fixed forwardRef issue

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
 import { forwardRef } from "react";
 
-const Thumbnail = forwardRef(({ result }, ref) => {
+const Thumbnail = ({ result }, ref) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
     <div
@@ -36,6 +36,6 @@ const Thumbnail = forwardRef(({ result }, ref) => {
       </div>
     </div>
   );
-});
+};
 
-export default Thumbnail;
+export default forwardRef(Thumbnail);
