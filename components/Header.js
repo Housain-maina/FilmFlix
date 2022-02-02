1b5e023f99c9a61976372f90c83aa47b29e5a79c
Hussaini Maina Usman <hussainmaina27@gmail.com>
2022-02-02 07:09:38 +0100

    initail commit

---
+++ components/Header.js
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import HeaderItem from "./HeaderItem";
+import Link from "next/link";
+import {
+  HomeIcon,
+  BadgeCheckIcon,
+  CollectionIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+
+export default function Header() {
+  return (
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
+
+      <Image
+        // layout="responsive"
+        className="object-conta"
+        src="https://links.papareact.com/ua6"
+        height={100}
+        width={200}
+        alt="logo"
+      />
+    </header>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: "jit",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        "3xl": "2000px",
+      },
+    },
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
+};
